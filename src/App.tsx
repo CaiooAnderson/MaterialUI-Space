59e6a351d0d137614f376537b807e6fdfbf2e038
Caio Anderson <caioandersongoes@gmail.com>
2025-05-19 12:31:24 -0300

    Novas rotas feitas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import SpaceView from './components/SpaceView';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Cursos from './components/Cursos';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SpaceView from "./components/SpaceView";
+import Cursos from "./components/Cursos";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<SpaceView />} />
-        <Route path="/cursos" element={<Cursos />} />
+        <Route path="/:planet/cursos" element={<Cursos />} />
+        <Route path="/:planet" element={<SpaceView />} />
       </Routes>
     </Router>
   );
